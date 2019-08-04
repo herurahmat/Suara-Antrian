ecaf9f2ea20d2ccdecdd65f659d6b74d3d47d480
Heru Rahmat Aknuari <eyubalzary@gmail.com>
2019-08-05 00:04:46 +0700

    Update Lagi

--- index.php
+++ index.php
@@ -88,7 +88,7 @@ require_once('function.php');
     
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
     
